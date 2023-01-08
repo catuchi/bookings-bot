7f52c7637a3b749f91a36eec25492bab71b641e6
Chibu <atuchibueze@gmail.com>
2023-01-07 19:33:02 -0500

    implement exit method

--- booking/booking.py
+++ booking/booking.py
@@ -4,10 +4,17 @@ from constants import BASE_URL
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r":/Users/catuchi/lighthouse/SeleniumDrivers"):
+    def __init__(
+        self, driver_path=r":/Users/catuchi/lighthouse/SeleniumDrivers", teardown=False
+    ):
         self.driver_path = driver_path
+        self.teardown = teardown
         os.environ["PATH"] += self.driver_path
         super(Booking, self).__init__()
+
+    def __exit__(self, exc_type, exc, traceback):
+        if self.teardown:
+            self.quit()
 
     def land_first_page(self):
         self.get(BASE_URL)
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from booking.booking import Booking
 
-inst = Booking()
-inst.land_first_page()
+# inst = Booking()
+# inst.land_first_page()
+
+with Booking() as bot:
+    bot.land_first_page()
+    print("Exiting ...")
